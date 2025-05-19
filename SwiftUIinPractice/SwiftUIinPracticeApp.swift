f8bd9c6113076eb13831ac7fea86462267dd784f
Phan Ngọc Vũ <phanngocvu89@gmail.com>
2025-05-20 02:32:53 +0900

    Initial Commit

---
+++ SwiftUIinPractice/SwiftUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIinPracticeApp.swift
+//  SwiftUIinPractice
+//
+//  Created by Phan Ngoc Vu on 2025/05/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIinPracticeTests/SwiftUIinPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIinPracticeTests.swift
+//  SwiftUIinPracticeTests
+//
+//  Created by Phan Ngoc Vu on 2025/05/20.
+//
+
+import Testing
+@testable import SwiftUIinPractice
+
+struct SwiftUIinPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
